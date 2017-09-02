34f825a2ac2ee7576b947198a1e593d3077c68b6
SaravananPushparaj <saravananpushparaj@gmail.com>
2017-09-02 10:02:12 +0530

    V1.0

---
+++ src/test/java/Demo/First_prg.java
@@ -0,0 +1,12 @@
+package Demo;
+
+public class First_prg {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Aug1 batch");
+
+		System.out.println("Hi");
+	}
+
+}
